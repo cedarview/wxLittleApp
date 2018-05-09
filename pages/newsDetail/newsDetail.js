183097c9f1f8594792d9610a38106cc69b8417d5
cedarview <cedarview@foxmail.com>
2018-05-09 21:36:48 +0800

    remove unsused function

--- pages/newsDetail/newsDetail.js
+++ pages/newsDetail/newsDetail.js
@@ -26,13 +26,6 @@ Page({
     })
     this.getNewsDetail(this.data.id);
   },
-  onPullDownRefresh() {
-    console.log("pull down new detail");
-    this.getNewsDetail(this.data.id, () => {
-      wx.stopPullDownRefresh()
-    });
-    //wx.stopPullDownRefresh();
-  },
 
   getNewsDetail: function (id,callback) {
     if(id==null||id.lenth<=0){
@@ -106,54 +99,5 @@ Page({
         }]
       }]
     })
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
+  }  
 })
